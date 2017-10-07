0aecc1db4ca10b5270ada825268fb936696705fb
Vipanpreetkaur <32308530+Vipanpreetkaur@users.noreply.github.com>
2017-10-07 15:09:23 -0400

    Add files via upload

--- login.php
+++ login.php
@@ -1,87 +1,69 @@
 <?php
-include('db.php');
-mysqli_select_db('payroll');
-if(isset($_POST['btnlogin']))
-{
-	$name=$_POST['gname'];
-	$pass=$_POST['gpass'];
+//include('database.php');
+   ob_start();
+   session_start();
+?>
+
+<?
+   // error_reporting(E_ALL);
+   // ini_set("display_errors", 1);
+?>
+
+<html lang = "en">
+   
+   <head>
+      <title>login</title>
+      <link href = "design.css" rel = "stylesheet">
+      
+   </head>
 	
-	$sql="select reguname,regpass from register where reguname='$name';";
-	//echo $sql;
-	$query=mysql_query($sql);
-	//echo $query;
-	while($row=mysql_fetch_array($query))
-	{
-		$gname=$row['reguname'];
-		$gpass=$row['regpass'];
-
-		echo $gname." "."".$gpass;
-			if($name==$gname && $pass==$gpass)
-			{
-				header('Location:admin.php');
-			}
-			else
-			{
-				echo "Invalid Credentials";
-			}
-		
-	}
-}
-?>
-<html>
-<head><title>Payroll Management System</title></head>
-<body>
-<Form name="loginform" method="POST">
-<div style="width:100%;height:100%">
-<!--Header-->
-<?php
-include_once("header.php");
-?>
-<!--Menu panel-->
-<div style="background-color:#999966;width:100%;height:5%;">
-
-<a href="login.php">Login</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-<a href="aboutus.php">About Us</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-</div>
-
-<!--Content-->
-<div style="background-color:#e6f2ff;width:1340px;height:500px;float:left">
-
-<br/><br/><br/><br/><br/><br/>
-<table border="1" align="center">
-<tr>
-<td colspan="2">
-<h3 align="center">Login</h3>
-</td>
-</tr>
-<tr>
-<td>
-Enter Username:</td><td><input type="text" name="gname" value="" required>
-</td>
-</tr>
-<tr>
-<td>
-Enter Password:</td><td><input type="password" name="gpass" value="" required>
-</td>
-</tr>
-<tr>
-<td colspan="2">
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<input type="Submit" name="btnlogin" value="Login" >
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<input type="reset" name="btncancel" value="cancel">
-</td>
-</tr>
-</table>
-<form>
-
-</div>
-<!--Footer-->
-<?php
-include_once("footer.php");
-?>
-</div>
-</body>
+   <body >
+      
+      <h2>Admin Login</h2> 
+      <div class = "container form-signin">
+         
+         <?php
+            $msg = '';
+            
+            if (isset($_POST['login']) && !empty($_POST['username']) 
+               && !empty($_POST['password'])) {
+				
+               if ($_POST['username'] == 'admin' && 
+                  $_POST['password'] == 'admin@123') {
+                  $_SESSION['valid'] = true;
+                  $_SESSION['timeout'] = time();
+                  $_SESSION['username'] = 'php';
+                  
+                  echo 'You have entered valid use name and password';
+               }else {
+                  $msg = 'Wrong username or password';
+               }
+            }
+         ?>
+      </div> <!-- /container -->
+      
+      <div class = "container">
+      
+         <form class = "form-signin" role = "form" 
+            action = "select.php" method = "post">
+            <fieldset>
+            <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
+            <table><tr><th>
+            <label for="name">Name</label></th>
+           <td> <input type = "text" class = "form-control" 
+               name = "username" placeholder = "username" 
+               required autofocus></br></td></tr>
+         <tr><th>      <label for="password">Password</label></th>
+           <td> <input type = "password" class = "form-control"
+               name = "password" placeholder = "password" required><br> </td></tr></table>
+            <button class = "btn btn-lg btn-primary btn-block" type = "submit"  
+               name = "login" >Login</button>
+            </fieldset>
+         </form>
+			
+         Click here to clean <a href = "logout.php" tite = "Logout">Session.
+         
+      </div> 
+      
+   </body>
 </html>
